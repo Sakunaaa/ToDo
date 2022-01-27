a3c6a9c1af2d01373bfaa177aa60924134e0ca3a
Sakunaaa <robertdaszkiewicz9@gmail.com>
2022-01-27 18:34:18 +0100

    Remove airbnb from eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'prettier', 'airbnb'],
+  extends: ['eslint:recommended', 'prettier'],
   plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 12,
